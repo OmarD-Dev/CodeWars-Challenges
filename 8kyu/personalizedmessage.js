c27b5d7f5125bb65c6eaa2de33fac2fc9f8493e4
Omar Daoud <omardaoud.dev@gmail.com>
2022-05-10 21:01:14 -0400

    Personalized Message

---
+++ 8kyu/personalizedmessage.js
@@ -0,0 +1,8 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+function greet (name, owner) {
+    if (name === owner){
+    return('Hello boss')
+  } else {
+    return ('Hello guest') 
+    }
+  }
