64f8e55c9c3de0bdc8c58116135cfff0de7bc063
Omar Daoud <omardaoud.dev@gmail.com>
2022-05-13 21:22:05 -0400

    Counting Sheep

---
+++ 8kyu/countingsheep.js
@@ -0,0 +1,4 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+function countSheeps(arrayOfSheep) {
+    return arrayOfSheep.filter(item => item === true).length
+   }
