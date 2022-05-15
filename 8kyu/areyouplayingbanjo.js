0ff80d0303f0f564b80d5ef64bba1e0264121b83
Omar Daoud <omardaoud.dev@gmail.com>
2022-05-14 23:44:12 -0400

    Are You Playing the Banjo

---
+++ 8kyu/areyouplayingbanjo.js
@@ -0,0 +1,6 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+let areYouPlayingBanjo = name => name.toLowerCase().split('')[0] == 'r'? name + ' plays banjo': name +' does not play banjo'
